85d0360b6401e7e7c995638d3f0f7c10bd2ba115
sccala <sccala18@gmail.com>
2021-10-12 16:04:36 -0700

    sidebar

---
+++ src/Feed.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Feed=()=> {
+    return (
+      <div>
+        {/*Header */}
+        Hello
+        {/*tweetBox */}
+        {/*Post */}
+        {/*Post */}
+        {/*Post */}
+        {/*Post */}
+        {/*Post */}
+      </div>
+    )
+}
+
+
